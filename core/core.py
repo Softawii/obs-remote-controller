746262704fc62e353c554c2c9efbd33271771602
Yan Figueiredo <yancfm@gmail.com>
2022-01-23 14:19:48 -0300

    fix: handling obs closing

--- core/core.py
+++ core/core.py
@@ -8,23 +8,35 @@ _parameters = simpleobsws.IdentificationParameters(ignoreNonFatalRequestChecks =
 _ws = simpleobsws.WebSocketClient(url = _config['obs_url'], 
                                  password = _config['obs_password'], 
                                  identification_parameters = _parameters) # Every possible argument has been passed, but none are required. See lib code for defaults.
-
+disconnect = True
 my_var = 0
 
-async def setup():
+async def setup(run_reconnect = True, log = True):
+    
+    global disconnect
+    
     try:
         await _ws.connect()
         await _ws.wait_until_identified() # Wait for the identification handshake to complete
+        disconnect = False
         print("Setup!")
     except:
-        print("Error!")
+        if log:
+            print("core.setup: OBS is not connected to the websocket or is closed")
+        disconnect = True 
+        if run_reconnect:
+            asyncio.ensure_future(reconnect())
         return False
     
     return True
             
             
-async def scene_list():
-    
+async def scene_list(): 
+    print("core.scene_list: trying to get scene list") 
+    print("core.scene_list: connection status = " + str(disconnect)) 
+    if disconnect:
+        return (False, [])
+      
     request = simpleobsws.Request('GetSceneList')
 
     try:
@@ -37,6 +49,9 @@ async def scene_list():
         return (False, [])
 
 async def set_scene(scene_name):
+    if disconnect:
+        return False
+    
     request = simpleobsws.Request('SetCurrentProgramScene', { 'sceneName': scene_name })
 
     try:
@@ -48,6 +63,10 @@ async def set_scene(scene_name):
 
 
 async def get_current_scene():
+    
+    if disconnect:
+        return (False, "")
+    
     request = simpleobsws.Request('GetCurrentProgramScene')
 
     try:
@@ -63,10 +82,25 @@ async def get_current_scene():
         return (False, "")
 
 async def call(request):
+    global disconnect
+    
     try:
         ret = await _ws.call(request)
         return ret
     except simpleobsws.NotIdentifiedError as e:
-        await setup()
+        print("core.call: OBS is not connected to the websocket or is closed")
+        disconnect = True 
+        asyncio.ensure_future(reconnect())
+        
+async def reconnect():
+    global disconnect
+    
+    print("core.reconnect: trying to Reconnect")
+    reconnected = False 
+    
+    while not reconnected:
+        reconnected = await setup(False, False)
         await asyncio.sleep(_config['update_time'])
-        await call(request)
+    disconnect = False
+    print("Reconnected")
+    
